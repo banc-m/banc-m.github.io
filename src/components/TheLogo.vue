ece5753e9fdc5596fd5495a4e1e9279504a37f2f
Martin Stewart <m11t@mm.st>
2026-01-26 17:45:23 +0000

    Update font family in TheLogo component, add Zen Dots font to index.html, and replace old CSS and JS assets with new versions.

--- src/components/TheLogo.vue
+++ src/components/TheLogo.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="m-auto d-md-flex justify-content-center align-items-center">
-        <img src="./../assets/old-dude.jpg" alt="logo" class="logo m-auto d-block d-md-inline-block me-md-4 shadow border" />
+        <img src="./../assets/old-dude.jpg" alt="logo" class="logo m-auto d-block d-md-inline-block me-md-4 shadow border border-3 border-secondary" />
         <h1 class="logo-text nx-grey d-md-inline-block">nuxmo</h1>
     </div>
 </template>
@@ -19,12 +19,11 @@ export default {
 }
 
 .logo-text {
-    // font-family: "Montserrat", sans-serif;
-    font-family: "Doto", sans-serif;
+    font-family: "Zen Dots", sans-serif;
     font-optical-sizing: auto;
-    font-weight: 600;
+    font-weight: 400;
     text-align: center;
-    font-size: 4.5rem;
+    font-size: 3.5rem;
     letter-spacing: 1rem;
     margin-right: -1rem;
     background: -webkit-linear-gradient(315deg, #e1a265 25%, #38bbe4);
